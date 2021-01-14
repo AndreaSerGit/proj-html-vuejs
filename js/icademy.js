43f36b61df6bd1753cdb1d435a19d258275ba68c
Andrea Serboli <andrea.serboli12@gmail.com>
2021-01-14 10:15:18 +0100

    Inizio vue

--- js/icademy.js
+++ js/icademy.js
@@ -1,7 +1,34 @@
 var app = new Vue(
   {
-    el: '',
+    el: '#container',
     data: {
+      listaLinkHeader: [
+        {
+          name: 'HOME',
+          link: 'https://iacademy.qodeinteractive.com/home-10/'
+        },
+        {
+          name: 'COURSES',
+          link: 'https://iacademy.qodeinteractive.com/courses/all-courses/'
+        },
+        {
+          name: 'INSTRUCTORS',
+          link: 'https://iacademy.qodeinteractive.com/become-an-instructor/'
+        },
+        {
+          name: 'EVENTS',
+          link: 'https://iacademy.qodeinteractive.com/events/2021-07/'
+        },
+        {
+          name: 'PAGES',
+          link: 'https://iacademy.qodeinteractive.com/about/'
+        },
+        {
+          name: 'ELEMENTS',
+          link: 'https://iacademy.qodeinteractive.com/elements/course-list/'
+        }
+
+      ]
 
     },
     created: function() {
