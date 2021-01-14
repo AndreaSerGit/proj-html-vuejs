3ca51ba2a9db6f9f77fa2a414ee46ccba142b227
Andrea Serboli <andrea.serboli12@gmail.com>
2021-01-14 10:39:28 +0100

    link footer

--- js/icademy.js
+++ js/icademy.js
@@ -5,7 +5,7 @@ var app = new Vue(
       listaLinkHeader: [
         {
           name: 'HOME',
-          link: 'https://iacademy.qodeinteractive.com/home-10/'
+          link: 'file:///C:/Users/Andre/Desktop/Boolean/Lavori/proj-html-vuejs/index.html'
         },
         {
           name: 'COURSES',
@@ -28,6 +28,42 @@ var app = new Vue(
           link: 'https://iacademy.qodeinteractive.com/elements/course-list/'
         }
 
+      ],
+      listaLinkSupport: [
+        {
+          name: 'User dashboard',
+          link: 'www.dashboard.com'
+        },
+        {
+          name: 'Contact Us',
+          link: 'www.contactus.com'
+        },
+        {
+          name: 'FAQ',
+          link: 'www.faq.com'
+        },
+        {
+          name: 'Course Offer',
+          link: 'www.offer.com'
+        },
+        {
+          name: 'Events',
+          link: 'www.events.com'
+        }
+      ],
+      listaPersonale: [
+        {
+          corso: 'Business English',
+          name: 'Preston Marshall'
+        },
+        {
+          corso: 'Social Computing',
+          name: 'David Sanders'
+        },
+        {
+          corso: 'Learn Spanish',
+          name: 'Jennie King'
+        },
       ]
 
     },
